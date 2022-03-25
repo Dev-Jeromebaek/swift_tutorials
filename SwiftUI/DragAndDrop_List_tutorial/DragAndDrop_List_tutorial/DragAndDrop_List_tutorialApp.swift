26ece99e3dc2c71fb1fb9f787d443724c4c99e69
seungyeub <superior051@icloud.com>
2022-03-25 17:31:19 +0900

    Add new project about SwiftUI Drag And Drop
    - drag and drop
    - DropDelegate

---
+++ SwiftUI/DragAndDrop_List_tutorial/DragAndDrop_List_tutorial/DragAndDrop_List_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragAndDrop_List_tutorialApp.swift
+//  DragAndDrop_List_tutorial
+//
+//  Created by 백승엽 on 2022/03/25.
+//
+
+import SwiftUI
+
+@main
+struct DragAndDrop_List_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
